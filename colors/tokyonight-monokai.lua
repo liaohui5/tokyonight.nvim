0b57c0449b45db782c29d02826b34cc6a9072b4f
secret <secretx500@qq.com>
2026-06-30 20:58:40 +0800

    fix: Pre-register monokai palette for runtimepath independence

--- colors/tokyonight-monokai.lua
+++ colors/tokyonight-monokai.lua
@@ -1,1 +1,11 @@
+-- Pre-register the monokai palette so Util.mod() can find it
+-- regardless of which runtimepath entry provides tokyonight core
+local modname = "tokyonight.colors.monokai"
+if not package.loaded[modname] then
+  local paths = vim.api.nvim_get_runtime_file("lua/tokyonight/colors/monokai.lua", false)
+  if #paths > 0 then
+    package.loaded[modname] = dofile(paths[1])
+  end
+end
+
 require("tokyonight").load({ style = "monokai" })
